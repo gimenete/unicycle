0ca8b66799ca3451cffd4d17a188c7bc3e5885b9
Alberto Gimeno <gimenete@gmail.com>
2017-09-22 11:03:03 +0200

    Loading spinner

--- src/app.tsx
+++ src/app.tsx
@@ -62,18 +62,4 @@ class App extends React.Component<any, AppState> {
   }
 }
 
-/*
-  <div id="open"></div>
-  <div id="loading">
-    <div class="pt-spinner pt-large">
-      <div class="pt-spinner-svg-container">
-        <svg viewBox="0 0 100 100">
-          <path class="pt-spinner-track" d="M 50,50 m 0,-44.5 a 44.5,44.5 0 1 1 0,89 a 44.5,44.5 0 1 1 0,-89"></path>
-          <path class="pt-spinner-head" d="M 94.5 50 A 44.5 44.5 0 0 0 50 5.5"></path>
-        </svg>
-      </div>
-    </div>
-  </div>
-*/
-
 ReactDOM.render(React.createElement(App, {}), document.getElementById('app'))
